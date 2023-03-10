c1e2770a16f91f98d2d0c413cb1c54d4c2a87e3a
Ajit Sahoo <87249806+Ajit99999@users.noreply.github.com>
2023-03-10 19:12:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ImageList from "./components/ImageList";
 
 function App() {
-
-  const [ imageLists , setImageLists  ] = useState([]);
+  const [imageLists, setImageLists] = useState(null);
   useEffect(() => {
     async function getPhotos() {
-      const res = await fetch(
-        `https://api.unsplash.com/photos/random?client_id=YSjOuRtoPY2FOELOJeYQOafXAE90f9Tgm2nwggOjECI&count=5`
-      );
-      const data = await res.json();
-      setImageLists(data)
+      try {
+        const res = await fetch(
+          `https://api.unsplash.com/photos/random?client_id=YSjOuRtoPY2FOELOJeYQOafXAE90f9Tgm2nwggOjECI&count=7`
+        );
+        const data = await res.json();
+        if (!res.ok) {
+          setImageLists([]);
+          return;
+        }
+        setImageLists(data);
+      } catch (err) {
+        setImageLists([]);
+        console.log(err.message);
+      }
     }
-    getPhotos()
+    getPhotos();
   }, []);
- 
+
   return (
     <div className="App font-bold">
-      <ImageList  imageLists={imageLists} />
+      <ImageList imageLists={imageLists} />
     </div>
   );
 }
